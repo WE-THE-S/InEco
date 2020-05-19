7e9f8234db1d1ed4ec073e1c3d2f1220e631a9ff
Gyeongsik Kim <gyungdal@the-s.kr>
2020-05-19 14:40:12 +0900

    테스트 구문 수정

--- test/packet_test.cpp
+++ test/packet_test.cpp
@@ -5,11 +5,10 @@
 using namespace std;
 
 void test_packet_size(void){
-    TEST_ASSERT_EQUAL(sizeof(base_message_t), 7);
-    TEST_ASSERT_EQUAL(sizeof(base_message_t), 7);
-    TEST_ASSERT_EQUAL(sizeof(led_message_t), 7);
-    TEST_ASSERT_EQUAL(sizeof(motor_message_t), 7);
-    TEST_ASSERT_EQUAL(sizeof(con_led_message_t), 7);
+    TEST_ASSERT_EQUAL(sizeof(base_message_t), 6);
+    TEST_ASSERT_EQUAL(sizeof(base_message_t), 6);
+    TEST_ASSERT_EQUAL(sizeof(led_message_t), 6);
+    TEST_ASSERT_EQUAL(sizeof(motor_message_t), 6);
     TEST_ASSERT_EQUAL(sizeof(device_communication_message_t), 8);
 }
 

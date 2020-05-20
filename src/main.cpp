2ab09b731fccd8a44a873a298a5c33fe30546ba1
Gyeongsik Kim <gyungdal@the-s.kr>
2020-05-20 16:34:16 +0900

    Update main.cpp
    async web server

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,9 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WebServer.h>
+#include <ESPmDNS.h>
+
 #include "./config.hpp"
 #include "./struct/packet.hpp"
 #include "./handler/led_handler.hpp"
@@ -15,6 +20,7 @@
   MotorHanlder motor;
   LedTranslate translate;
 #elif CONTROL_BOARD == 1
+  WebServer server(80);
   ControlTranslate translate;
   Alarm ledAlarm;
   WaterLevel waterLevel;
@@ -26,6 +32,28 @@ void setup() {
     instance->add(&led);
     instance->add(&motor);
   #elif CONTROL_BOARD == 1
+    Serial.begin(115200);
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(SSID, PASSWORD);
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(100);
+    }
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+
+    MDNS.begin("ineco");
+
+    server.on("/motor/on", []() {
+      server.send(200, "text/plain", "hello from esp32!");
+    });
+
+    server.on("/motor/set/{}", []() {
+      std::String arg = server.pathArg(0);
+      long time = atol(arg.c_str());
+      server.send(200, "text/plain", "User: '" + arg + "'");
+    });
+  
+    server.begin();
     auto instance = Broadcast<service_signal_t>::getInstance();
     instance->add(&ledAlarm);
     instance->add(&waterLevel);
@@ -36,6 +64,7 @@ void loop() {
   #if LED_BOARD == 1
     translate.recv();
   #elif CONTROL_BOARD == 1
+    server.handleClient();
     waterLevel.execute();
   #endif
 }

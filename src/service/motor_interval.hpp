e7fb3f67882c893cc3d3bf880bbc17131f42a27d
Gyeongsik Kim <gyungdal@the-s.kr>
2020-05-21 09:40:52 +0900

    Update motor_interval.hpp
    broadcast 메세지 핸들러 추가

--- src/service/motor_interval.hpp
+++ src/service/motor_interval.hpp
@@ -1,8 +1,11 @@
 #ifndef __MOTOR_INTERVAL_HPP__
 #define __MOTOR_INTERVAL_HPP__
 
-#include "../struct/service.hpp"
 #include <Arduino.h>
+#include "../struct/packet.hpp"
+#include "../struct/service.hpp"
+#include "../utils/broadcast.hpp"
+
 class MotorInterval : public Service {
     private:
         //기간
@@ -15,7 +18,7 @@ class MotorInterval : public Service {
         uint64_t lastTime;
 
         //마지막으로 타이머가 설정한 값
-        bool lastIntervalStatus;
+        MOTOR_STATUS lastIntervalStatus;
 
         //반복 실행 설정
         bool intervalEnable; 
@@ -23,15 +26,52 @@ class MotorInterval : public Service {
         //onOff
         bool onOff;
     public:
+
     MotorInterval() {
 
     }
+
     void execute(){
-        
+        if(this->intervalEnable){
+            const uint64_t const now = millis();
+            const auto requireOffTime = this->lastTime + this->intervalSpan;
+            const auto requireOnTime = this->lastTime + this->intervalTime;
+            service_signal_t signal;
+            signal.type = SERVICE_SIGNAL_TYPE::PACKET_SEND;
+            motor_message_t* motorMessage = new motor_message_t;
+            switch(this->lastIntervalStatus){
+                case MOTOR_STATUS::MOTOR_OFF : {
+                    if(requireOnTime > now){
+                        motorMessage->status = MOTOR_STATUS::MOTOR_ON;
+                        communcation_service_signal_t communicationSignal;
+                        Broadcast<service_signal_t>::getInstance()->broadcast(signal);
+                    }
+                    break;
+                }
+                case MOTOR_STATUS::MOTOR_ON : {
+                    
+                    break;
+                }
+            }
+            delete motorMessage;
+        }
     }
 
     void onMessage(const service_signal_t message){
         if(message.type == SERVICE_SIGNAL_TYPE::MOTOR_INTERVAL_SET){
+            motor_interval_service_signal_t signal;
+            signal.value = message.value;
+            if(signal.isIntervalSet){
+                this->intervalSpan = signal.intervalSpan;
+                this->intervalTime = signal.intervalTime;
+                if(signal.intervalEnable != this->intervalEnable){
+                    this->lastIntervalStatus = MOTOR_STATUS::MOTOR_OFF;
+                    this->lastTime = (millis() - signal.intervalTime);
+                }
+                this->intervalEnable = signal.intervalEnable;
+            }else{
+                this->onOff = signal.onOff;
+            }
         }
     }
 };

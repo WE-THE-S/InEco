e7a02810b4a2c9a429dfabb61246bc194df1c194
Gyeongsik Kim <gyungdal@the-s.kr>
2020-05-21 10:02:57 +0900

    Update control_translate.hpp
    패킷 해더 설정 로직 수정

--- src/translate/control_translate.hpp
+++ src/translate/control_translate.hpp
@@ -27,12 +27,12 @@ class ControlTranslate : protected Translate, public ServiceSignalBroadcastRecei
                 communcation_service_signal_t signal;
                 device_communication_message_t packet;
                 
-                signal.value = message.value;
-                packet.message = *signal.message;
                 uint8_t type = static_cast<uint8_t>(packet.type);
                 uint8_t dir = static_cast<uint8_t>(packet.type);
                 
-                packet.type = static_cast<MESSAGE_TYPE>(type | dir);   
+                signal.value = message.value;
+                packet.message = *signal.message;
+                packet.header = (type | dir);   
                 packet.crc = packet.getCrc();
                 
                 this->bottom->write(packet.bytes, sizeof(device_communication_message_t));

ce48f9ac7fe64bff4c8f6f89b247cf5096ff0700
Kim Gyeongsik <gyungdal@the-s.kr>
2020-12-18 08:05:40 +0900

    bme 에러 수정

--- src/service/air_sensor.hpp
+++ src/service/air_sensor.hpp
@@ -17,12 +17,12 @@
 #include <DFRobot_BME280.h>
 #include <DFRobot_CCS811.h>
 
-static DFRobot_BME280_IIC bme(&Wire, BME280_ADDRESS);
 	
 //공기 센서 
 class AirSensor : public Service {
 private:
 //마지막으로 보낸 현재 공기
+	DFRobot_BME280_IIC bme;
 	air_sensor_service_signal_t last;
 	DFRobot_CCS811 ccs;
 
@@ -38,33 +38,41 @@ private:
 		Broadcast<service_signal_t>::getInstance()->broadcast(signal);
 	}
 
-public:
-	//i2c 인스턴스 초기화
-	AirSensor() {
+	void init() {
 		bme.reset();
-		while(bme.begin() != DFRobot_BME280::eStatus_t::eStatusOK) {
-			ESP_LOGI(typename(this), "BME280 INIT FAIL");
-			delay(100);
-		}
-		while(ccs.begin() != 0){
-			ESP_LOGI(typename(this), "CCS INIT FAIL");
-			delay(100);
-		}
+		bme.begin();
+		ccs.begin();
     	ccs.setMeasCycle(ccs.eCycle_250ms);
 	}
 
+public:
+	//i2c 인스턴스 초기화
+	AirSensor() : bme(&Wire, BME280_ADDRESS) {
+		ESP_LOGI(typename(this), "sda : %u", SDA);
+		ESP_LOGI(typename(this), "scl : %u", SCL);
+		init();
+	}
+
 	void execute() {
-		if(ccs.checkDataReady()){
-			const auto co2 = ccs.getCO2PPM();
-			const auto tvoc = ccs.getTVOCPPB();
-			const auto temp = bme.getTemperature();
-			const auto humi = bme.getHumidity();
-			last.co2 = co2;
-			last.tvoc = tvoc;
-			last.temp = static_cast<int8_t>(temp);
-			last.humi = static_cast<uint8_t>(humi);
-			send(last);
+		init();
+		const auto start = millis() + 400;
+		while(!ccs.checkDataReady()){
+			if(millis() > start){
+				break;
+			}
+			delay(10);
 		}
+		const auto temp = 0; // bme.getTemperature();
+		const auto humi = 0; // bme.getHumidity();
+		const auto co2 = ccs.getCO2PPM();
+		const auto tvoc = ccs.getTVOCPPB();
+
+		last.co2 = co2;
+		last.tvoc = tvoc;
+		last.temp = static_cast<int8_t>(temp);
+		last.humi = static_cast<uint8_t>(humi);
+		send(last);
+	
 	}
 
 	void onMessage(const service_signal_t message) {
